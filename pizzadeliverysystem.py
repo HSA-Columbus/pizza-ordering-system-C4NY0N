f3ac57c8dc32953b2463d5b4bcca436e350d21d4
Canyon.Jeffers <student.c.jeffers@horizoncolumbus.org>
2020-02-14 09:23:32 -0500

    Database Work

--- pizzadeliverysystem.py
+++ pizzadeliverysystem.py
@@ -1,12 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_from_directory
+import sqlite3
 
-application = Flask(__name__)
+application = Flask(__name__, static_url_path='/static')
 
 
 @application.route("/")
 def index():
-    return render_template("htmlkode.html")
+    with sqlite3.connect(("data.db")) as conn:
+        command = "Select * From table"
+        conn.execute(command)
+        table = conn.execute(command)
+        assignment_list = table.fetchall()
+        return render_template("htmlkode.html")
 
 
-if __name__ == "__main__":
+@application.route("/htmlkode", methods=('get', 'POST'))
+def htmlkode():
+    if request.method == "POST":
+        with sqlite3.connect("data.db") as conn:
+            command = "INSERT INTO pizza VALUES (?, ?, ?,)"
+        list = []
+        list.append(request.form['radio1'])
+        list.append(request.form['Green Pepper'])
+        list.append(request.form['Mushroom'])
+        list.append(request.form['Tomato'])
+        list.append(request.form['Pickles'])
+        list.append(request.form['Olives'])
+        list.append(request.form['UnitPrice'])
+        list.append(request.form['UnitNumber'])
+        list.append(request.form['Totals'])
+    conn.execute(command, list)
+    conn.commit()
+    return render_template(htmlkode.html)
+
+
+   if __name__ == "__main__":
     application.run(debug=True)
