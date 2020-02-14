3072b787bab8d2f33ee6a73ca07f34db9d509012
Canyon.Jeffers <student.c.jeffers@horizoncolumbus.org>
2020-02-14 15:40:47 -0500

    Project Finished

--- pizzadeliverysystem.py
+++ pizzadeliverysystem.py
@@ -4,35 +4,37 @@ import sqlite3
 application = Flask(__name__, static_url_path='/static')
 
 
-@application.route("/")
+@application.route("/cart")
 def index():
-    with sqlite3.connect(("data.db")) as conn:
-        command = "Select * From table"
-        conn.execute(command)
+    with sqlite3.connect(("data")) as conn:
+        command = "Select * From pizza"
         table = conn.execute(command)
         assignment_list = table.fetchall()
-        return render_template("htmlkode.html")
+        return render_template("cart.html", orders=assignment_list)
 
 
+@application.route("/")
 @application.route("/htmlkode", methods=('get', 'POST'))
 def htmlkode():
     if request.method == "POST":
-        with sqlite3.connect("data.db") as conn:
-            command = "INSERT INTO pizza VALUES (?, ?, ?,)"
+        with sqlite3.connect("data") as conn:
+            command = "INSERT INTO pizza VALUES (?, ?, ?, ?, ?)"
         list = []
+        additions = ""
+        additions += request.form['Green_Pepper'] if request.form.get("Green_Pepper", None) is not None else " "
+        additions += request.form['Mushroom'] if request.form.get("Mushroom", None) is not None else " "
+        additions += request.form['Tomato'] if request.form.get("Tomato", None) is not None else " "
+        additions += request.form['Pickles'] if request.form.get("Pickles", None) is not None else " "
+        additions += request.form['Olives'] if request.form.get("Olives", None) is not None else " "
         list.append(request.form['radio1'])
-        list.append(request.form['Green Pepper'])
-        list.append(request.form['Mushroom'])
-        list.append(request.form['Tomato'])
-        list.append(request.form['Pickles'])
-        list.append(request.form['Olives'])
-        list.append(request.form['UnitPrice'])
+        list.append(additions)
         list.append(request.form['UnitNumber'])
+        list.append(request.form['UnitPrice'])
         list.append(request.form['Totals'])
-    conn.execute(command, list)
-    conn.commit()
-    return render_template(htmlkode.html)
+        conn.execute(command, list)
+        conn.commit()
+    return render_template("htmlkode.html")
 
 
-   if __name__ == "__main__":
+if __name__ == "__main__":
     application.run(debug=True)
